c681ef9c114b58b436c51d6a09d46b413b693e6b
Haihua Yang <yanghh@gmail.com>
2022-12-08 00:38:22 -0800

    fix dependency in Makefile

--- functions.mk
+++ functions.mk
@@ -20,8 +20,8 @@ $(1)/$(2)/$(3)_asn1-priv.h: $(1)/$(2)/$(3)_asn1_files
 $(1)/$(2)/$(3)_asn1.c: $(1)/$(2)/$(3)_asn1_files
 	cp $(1)/$(2)/asn1_$(3)_asn1.x $(1)/$(2)/$(3)_asn1.c
 
-$(1)/$(2)/$(3)_asn1_files: $(2)/$(3).asn1
-	cd $(1)/$(2); $(ASN1_COMPILE) --one-code-file $(ASN1_OPT_$(3)) $$($(3)_path) $(3)_asn1
+$(1)/$(2)/$(3)_asn1_files: $(2)/$(3).asn1 | $(1)/$(2)
+	cd $(1)/$(2) && $(ASN1_COMPILE) --one-code-file $(ASN1_OPT_$(3)) $$($(3)_path) $(3)_asn1
 endef
 
 
